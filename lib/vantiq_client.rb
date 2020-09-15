a1db7ecaab09b13a532e0214c97b337f9a461b85
Yuka Okabe <32320352+yukabeoka@users.noreply.github.com>
2020-09-15 17:55:19 +0900

    最新のvantiq_clientに併せる

--- lib/vantiq_client.rb
+++ lib/vantiq_client.rb
@@ -1,12 +1,27 @@
 require 'vantiq_client/version'
 require 'json'
 
-
 module VantiqClient
   class Error < StandardError; end
 
   def initialize(faraday_client: nil, vantiq_url)
     @faraday_client = faraday_client || Faraday.new(url: "https://#{vantiq_url}")
+  end
+
+  def index(model_alias)
+    res = @faraday_client.get do |req|
+      http_headers(req)
+      req.url(model_alias.to_s)
+    end
+    res
+  end
+
+  def show(model_alias, id)
+    res = @faraday_client.get do |req|
+      http_headers(req)
+      req.url("#{model_alias}?where={\"id\":#{id}}")
+    end
+    res
   end
 
   def create(model_alias, recodes, vantiq_token)
@@ -15,7 +30,7 @@ module VantiqClient
       req.url(model_alias.to_s)
       req.body = recodes.to_json
     end
-    JSON.parse(res.body)
+    res
   end
 
   def update(model_alias, id, diff, vantiq_token)
@@ -24,7 +39,7 @@ module VantiqClient
       req.url("#{model_alias}?where={\"id\":#{id}}")
       req.body = diff.to_json
     end
-    JSON.parse(res.body)
+    res
   end
 
   def delete(model_alias, id, vantiq_token)
@@ -32,7 +47,7 @@ module VantiqClient
       http_headers(req, vantiq_token)
       req.url("#{model_alias}?where={\"id\":#{id}}")
     end
-    JSON.parse(res.body)
+    res
   end
 
   private
