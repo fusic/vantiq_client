6f690edb5d0a17cf9c602df47800c5cd8f357fc0
Yuka Okabe <32320352+yukabeoka@users.noreply.github.com>
2020-09-17 12:27:48 +0900

    class追加

--- lib/vantiq_client.rb
+++ lib/vantiq_client.rb
@@ -1,63 +1,65 @@
 require 'vantiq_client/version'
 require 'json'
 
-module VantiqClient
+module Vantiq
   class Error < StandardError; end
+  class Client
 
-  def initialize(vantiq_url, faraday_client: nil)
-    @faraday_client = faraday_client || Faraday.new(url: "https://#{vantiq_url}")
-  end
-
-  def index(model_alias)
-    res = @faraday_client.get do |req|
-      http_headers(req)
-      req.url(model_alias.to_s)
+    def initialize(vantiq_url, faraday_client: nil)
+      @faraday_client = faraday_client || Faraday.new(url: "https://#{vantiq_url}")
     end
-    res
-  end
 
-  def show(model_alias, id)
-    res = @faraday_client.get do |req|
-      http_headers(req)
-      req.url("#{model_alias}?where={\"id\":#{id}}")
+    def index(model_alias)
+      res = @faraday_client.get do |req|
+        http_headers(req)
+        req.url(model_alias.to_s)
+      end
+      res
     end
-    res
-  end
 
-  def create(model_alias, recodes, vantiq_token)
-    res = @faraday_client.post do |req|
-      http_headers(req, vantiq_token)
-      req.url(model_alias.to_s)
-      req.body = recodes.to_json
+    def show(model_alias, id)
+      res = @faraday_client.get do |req|
+        http_headers(req)
+        req.url("#{model_alias}?where={\"id\":#{id}}")
+      end
+      res
     end
-    res
-  end
 
-  def update(model_alias, id, diff, vantiq_token)
-    res = @faraday_client.put do |req|
-      http_headers(req, vantiq_token)
-      req.url("#{model_alias}?where={\"id\":#{id}}")
-      req.body = diff.to_json
+    def create(model_alias, recodes, vantiq_token)
+      res = @faraday_client.post do |req|
+        http_headers(req, vantiq_token)
+        req.url(model_alias.to_s)
+        req.body = recodes.to_json
+      end
+      res
     end
-    res
-  end
 
-  def delete(model_alias, id, vantiq_token)
-    res = @faraday_client.delete do |req|
-      http_headers(req, vantiq_token)
-      req.url("#{model_alias}?where={\"id\":#{id}}")
+    def update(model_alias, id, diff, vantiq_token)
+      res = @faraday_client.put do |req|
+        http_headers(req, vantiq_token)
+        req.url("#{model_alias}?where={\"id\":#{id}}")
+        req.body = diff.to_json
+      end
+      res
     end
-    res
-  end
 
-  private
+    def delete(model_alias, id, vantiq_token)
+      res = @faraday_client.delete do |req|
+        http_headers(req, vantiq_token)
+        req.url("#{model_alias}?where={\"id\":#{id}}")
+      end
+      res
+    end
 
-  def http_headers(req, vantiq_token)
-    [
-      ['Authorization', "Bearer #{vantiq_token}"],
-      ['Content-Type', 'application/json']
-    ].each do |key, val|
-      req.headers[key] = val
+    private
+
+    def http_headers(req, vantiq_token)
+      [
+        ['Authorization', "Bearer #{vantiq_token}"],
+        ['Content-Type', 'application/json']
+      ].each do |key, val|
+        req.headers[key] = val
+      end
     end
   end
 end
